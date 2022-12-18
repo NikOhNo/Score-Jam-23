046baa8120f61eff59ac4bd9b51c82d485030867
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-12-18 02:08:08 -0800

    Improved Shooting System

--- Score Jam 23/Assets/Scripts/PlayerShooting.cs
+++ Score Jam 23/Assets/Scripts/PlayerShooting.cs
@@ -8,14 +8,16 @@ public class PlayerShooting : Shooting
     { 
         if (Mathf.Abs(Input.GetAxisRaw("Fire1")) >= Mathf.Epsilon && canShoot)
         {
-            float direction = GetComponent<PlayerMovement>().GetFaceDirection();
-            GetComponent<Animator>().SetBool("Shoot", true);
+            Shoot();
         }
     }
     public override void Shoot(float direction = 1)
     {
         direction = FindObjectOfType<PlayerMovement>().GetFaceDirection();
         base.Shoot(direction);
+
+        GetComponent<Animator>().SetBool("Shoot", true);
+        GetComponent<Animator>().SetTrigger("Shoot Trig");
     }
 
     public void EndShootAnim()
