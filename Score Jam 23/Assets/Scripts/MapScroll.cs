10f1308fcaf341f18b3fdd7a50b7288e950c5bd0
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-12-17 18:49:38 -0800

    Background Now Scrolls

--- Score Jam 23/Assets/Scripts/MapScroll.cs
+++ Score Jam 23/Assets/Scripts/MapScroll.cs
@@ -8,23 +8,29 @@ public class MapScroll : MonoBehaviour
     public static MapScroll instance;
 
     [SerializeField]
-    float scrollSpeed = -1f;
+    float characterScrollSpeed = -1f;
 
-    Rigidbody2D myRb;
+    [SerializeField]
+    float backgroundScrollSpeed = 0.2f;
+
+    private MeshRenderer myRenderer;
+    private float timeElapsed = 0f;
 
     private void Start()
     {
-        myRb = GetComponent<Rigidbody2D>();
+        //myRb = GetComponent<Rigidbody2D>();
+        myRenderer = GetComponent<MeshRenderer>();
         instance = this;
     }
 
     private void Update()
     {
-        myRb.velocity = new Vector2(scrollSpeed, 0f);
+        timeElapsed += Time.deltaTime;
+        myRenderer.material.mainTextureOffset = new Vector2(timeElapsed * backgroundScrollSpeed, 0f);
     }
 
     public float GetScrollSpeed()
     {
-        return scrollSpeed;
+        return characterScrollSpeed;
     }
 }
